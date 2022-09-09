38855ba03fcd1c0b46a6d78a2776c59ab92884a7
shiva <shiva@gmail.com>
2022-09-09 13:09:17 +0530

    department-forms aditya

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DashboardScreen from './Screens/DashboardScreen/DashboardScreen';
+import ScrutityDashScreen from './Screens/DashboardScreen/ScrutinyFormScreen/Scrutinyform';
 import {Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     
       <Routes>
-        <Route path="/dashboard" element={<DashboardScreen />}>
-        </Route>
+        <Route path="/dashboard" element={<DashboardScreen />}></Route>
+        <Route path="/department-dashboard" element={<ScrutityDashScreen />}></Route>
       </Routes>
     
     
--- src/components/forms/Applicantform.js
+++ src/components/forms/Applicantform.js
@@ -1,5 +1,4 @@
 import React,{useState,useEffect}from "react";
-import { Component } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Card, Row, Col} from "react-bootstrap";
 
